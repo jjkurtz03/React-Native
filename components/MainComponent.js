754e24521d5af46d9e90be673d837b7f5607d88b
Josh Kurtz <jjkurtz03@gmail.com>
2021-09-20 16:04:57 -0500

    added additional login component functionality

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -320,13 +320,15 @@ class Main extends Component {
         this.props.fetchComments();
         this.props.fetchPromotions();
         this.props.fetchPartners();
+        this.showNetInfo();
+    }
 
-        NetInfo.fetch().then(connectionInfo => {
+        async showNetInfo() {
+            let connectionInfo = await NetInfo.fetch();
             (Platform.OS === 'ios')
                 ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
                 : ToastAndroid.show('Initial Network Connectivity Type: ' +
                     connectionInfo.type, ToastAndroid.LONG);
-        });
 
         this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
             this.handleConnectivityChange(connectionInfo);
